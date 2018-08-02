b51860a94e581bb8eabfab89cf89407a7550a2fe
Robert Scarth <robertscarthgithub443952@gmail.com>
2018-08-02 22:29:00 +0100

    First iteration of code
    R code to read in the data .txt files

--- Code/ReadInData.R
+++ Code/ReadInData.R
@@ -8,7 +8,15 @@ indexFileName = "Index"
 
 index = read_tsv( paste( baseFilePath , indexFileName , sep="" ) )
 
-i = 1
-relativeFilePath = paste( index$Show[[i]] , index$SectionA[[i]] , index$SectionB[[i]] , index$Prize[[i]] , sep="/" )
-
-prizeWinners = read_tsv( paste( baseFilePath , relativeFilePath , ".txt" , sep="" ) )
+prizeWinners = filter( tibble(Year=0,Exhibitor="",Champion="",Name="",Notes="",Show="",SectionA="",SectionB="",Prize="") , FALSE )
+for(i in c(1:nrow(index))){
+  relativeFilePath = paste( index$Show[[i]] , index$SectionA[[i]] , index$SectionB[[i]] , index$Prize[[i]] , sep="/" )
+  pW = read_tsv( paste( baseFilePath , relativeFilePath , ".txt" , sep="" ) )
+  pW = pW %>% mutate(
+      Show = index$Show[[i]],
+      SectionA = index$SectionA[[i]],
+      SectionB = index$SectionB[[i]],
+      Prize = index$Prize[[i]]
+  )
+  prizeWinners = union(prizeWinners,pW)
+}
