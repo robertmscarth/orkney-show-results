19a594cecdec954450aa1fa18918cccb1f7a9dcb
Robert Scarth <robertscarthgithub443952@gmail.com>
2018-08-03 20:57:42 +0100

    Added comments and code to output result
    Added some comments
    Added code to output the result of combining all the data. The output is to a tab separated text file.

--- Code/ReadInData.R
+++ Code/ReadInData.R
@@ -1,14 +1,20 @@
 # load in the tidyverse library
 library(tidyverse)
 
+# This code asssumes that the working directory is set to the folder containing all the Orkney Show Results files and subfolders
 wd = getwd()
 dataFolder = "/Data/"
 baseFilePath = paste( wd , dataFolder , sep="" )
 indexFileName = "Index"
+outputFolder = "/Data/Generated/"
+outputFileName = "AllPrizeWinners.txt"
 
+# Read in the index file which lists the text files containing the results
 index = read_tsv( paste( baseFilePath , indexFileName , sep="" ) )
 
+# Create an empty tibble with the required columns
 prizeWinners = filter( tibble(Year=0,Exhibitor="",Champion="",Name="",Notes="",Show="",SectionA="",SectionB="",Prize="") , FALSE )
+# Loop through the files listed in the index, read them in, and add them to the tibble just created
 for(i in c(1:nrow(index))){
   relativeFilePath = paste( index$Show[[i]] , index$SectionA[[i]] , index$SectionB[[i]] , index$Prize[[i]] , sep="/" )
   pW = read_tsv( paste( baseFilePath , relativeFilePath , ".txt" , sep="" ) )
@@ -20,3 +26,7 @@ for(i in c(1:nrow(index))){
   )
   prizeWinners = union(prizeWinners,pW)
 }
+
+# Write out the full set of prize winners to a text file, with variables separated by tabs
+outputFilePath = paste( wd , outputFolder , outputFileName , sep="" )
+write_tsv( prizeWinners , outputFilePath )
